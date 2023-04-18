c0b10ed41dbf5ac06f4847f41479bdb1dba07b75
Giovannaabruno <91497382+Giovannaabruno@users.noreply.github.com>
2023-04-18 00:17:25 -0400

    Add files via upload

---
+++ src/controller/Runner.java
@@ -0,0 +1,48 @@
+package controller;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import view.gui.CollagingView;
+import view.gui.JFrameView;
+import view.textui.Textui;
+
+/**
+ * Runner class, This class run the GUI view and Text view,
+ * to run the code in the view. And uses the
+ * command line option to display it
+ */
+public class Runner {
+
+  /**
+   * Main Methode, the entry point of an executable program;
+   * it is where the program control starts and ends.
+   *
+   * @param args arguments
+   */
+  public static void main(String[] args) throws IOException {
+    String command;
+    ImageController imgCtr = new ImageController();
+    Scanner scan = null;
+    if (args.length == 0) {
+      /// run a GUI
+      CollagingView view = new JFrameView(imgCtr);
+      return;
+    } else if (args.length == 1 && args[0].equals("-text")) {
+      // interactive mode
+      scan = new Scanner(System.in);
+    } else if (args.length == 2 && args[0].equals("-file")) {
+      // run in file modes
+      scan = new Scanner(new File(args[1]));
+    }
+    if (scan != null) {
+      Textui textui = new Textui(imgCtr);
+      while (scan.hasNextLine()) {
+        command = scan.nextLine();
+        textui.readCommand(command);
+      }
+    }
+  }
+}
